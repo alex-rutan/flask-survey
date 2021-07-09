f6d838a776ae767c5c3e06d7079858284b46547c
Alex  Rutan <alexrutan04@gmail.com>
2021-07-08 17:54:55 -0700

    last finishing touches

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
+RESPONSES_KEY_NAME = "responses"
+
 
 @app.route('/')
 def load_home():
@@ -23,7 +25,7 @@ def load_home():
 def start_survey():
     """Redirects to the first question of the survey"""
 
-    session["responses"] = []
+    session[RESPONSES_KEY_NAME] = []
 
     return redirect("/questions/0")
 
@@ -33,14 +35,16 @@ def start_survey():
 def ask_questions(question_num):
     """Loads pages for each question in the survey, and logs answer in responses list"""
 
-    responses_length = len(session['responses'])
+    responses_length = len(session[RESPONSES_KEY_NAME])
 
-    # If the user has completed all questions and tries to return to a survey question, they will be redirected to the thanks endpoint
+    # If the user has completed all questions and tries to return to a survey question,
+    # they will be redirected to the thanks endpoint
     if responses_length == len(survey.questions):
         flash('You have already completed the survey.')
         return redirect('/thanks')
 
-    # If the user tries to skip ahead to a further question, they will be redirected to the correct question
+    # If the user tries to skip ahead or go back to another question to a further question,
+    # they will be redirected to the correct question
     if question_num != responses_length:
         flash('You tried to access an invalid question. Bad you.')
         return redirect(f'/questions/{responses_length}')
@@ -53,12 +57,14 @@ def ask_questions(question_num):
 
 @app.route('/answer', methods=['POST'])
 def handle_answer():
-    """Appends the user's answer to the session and redirects the user to the next question, or thanks page if they've completed all questions"""
+    """Appends the user's answer to the session and redirects the user to the next question,
+     or thanks page if they've completed all questions"""
     answer = request.form['answer']
 
-    responses = session["responses"]
+    # TODO KEEP THESE LINES INTACT - Will implement database later
+    responses = session[RESPONSES_KEY_NAME] 
     responses.append(answer)
-    session["responses"] = responses
+    session[RESPONSES_KEY_NAME] = responses
 
     if len(responses) == len(survey.questions):
         return redirect('/thanks')
